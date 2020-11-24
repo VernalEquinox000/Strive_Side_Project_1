bc6184a8ef06d7dbb167d7158fc67320bb8e2989
AnriB <49607649+AnriB@users.noreply.github.com>
2020-11-24 23:26:56 +0100

    123

--- src/components/Backoffice.jsx
+++ src/components/Backoffice.jsx
@@ -1,9 +1,61 @@
 import React, { Component } from "react";
+import {Form, Button} from "react-bootstrap";
 
 class Backoffice extends Component {
-  render() {
-    return <div>Backoffice</div>;
-  }
+    state = {
+        product: {
+            name: '',
+            description: '',
+            price: 0,
+            imageURL: '' 
+        }
+    }
+    onChangeHa
+    submitData = async () => {
+        try {
+            await fetch (`https://striveschool-api.herokuapp.com/api/product/`,
+            {method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmI2OGEwNjk4MzViMDAwMTc1ODRmMzMiLCJpYXQiOjE2MDU3OTg0MDcsImV4cCI6MTYwNzAwODAwN30.YMFEHuLKmsTiRw_58rtTkNg5n_1jZGZdmOubN1Oo9O0'
+                },
+            body: JSON.stringify(this.value.state)   
+            });
+
+        }
+        catch(e) {
+            
+        }
+    }
+    render() {
+        return (
+        <Form>
+            <Form.Group>
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" value ={this.state.product.name} placeholder="Name" required onChange={(e) => this.setState({ product: { ...this.state.product, name: e.target.value } })} />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Description</Form.Label>
+              <Form.Control type="text" value ={this.state.product.description} placeholder="Description" required onChange={(e) => this.setState({ product: { ...this.state.product, description: e.target.value } })} />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Brand</Form.Label>
+              <Form.Control type="text" value ={this.state.product.brand} placeholder="Brand" required onChange={(e) => this.setState({ product: { ...this.state.product, brand: e.target.value } })} />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Price</Form.Label>
+              <Form.Control type="number" value ={this.state.product.price} placeholder="Price" required onChange={(e) => this.setState({ product: { ...this.state.product, price: e.target.value } })} />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>ImageURL</Form.Label>
+              <Form.Control type="url" value ={this.state.product.imageURL} placeholder="ImageURL" required onChange={(e) => this.setState({ product: { ...this.state.product, ImageURL: e.target.value } })} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+        </Form>
+        )
+    }
 }
 
 export default Backoffice;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
 
 class Home extends Component {
-  render() {
-    console.log(this.props)
-    return <div>Home</div>;
-  }
+
+    state = {
+        products: []
+    };
+    fetchProducts = async () => {
+        try {
+            let response = await fetch(`https://striveschool-api.herokuapp.com/api/product/`, 
+            {headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmI2OGEwNjk4MzViMDAwMTc1ODRmMzMiLCJpYXQiOjE2MDU3OTg0MDcsImV4cCI6MTYwNzAwODAwN30.YMFEHuLKmsTiRw_58rtTkNg5n_1jZGZdmOubN1Oo9O0"
+            }
+            });
+            let data = await response.json();
+            this.setState({products: data})
+            console.log(data)
+        }
+        catch(e) {
+            console.log(e)
+        }
+    }
+
+    componentDidMount = () => {
+        this.fetchProducts();
+    }
+
+    render() {
+        console.log(this.props)
+        return <div>
+            {this.state.products.map(product => <span>{product.name}</span>)}
+        </div>;
+    }
 }
 
 export default Home;
